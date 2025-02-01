04f8abb12d8b1058216d6a07aec32e0454dc1744
Mohamed Siblini <msiblini@gmail.com>
2025-01-31 19:18:12 -0500

    Comment out event handler association for PC platform in commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -74,6 +74,6 @@ function getDomain(email) {
 }
 
 // IMPORTANT: To ensure your add-in is supported in the Outlook client on Windows, remember to map the event handler name specified in the manifest to its JavaScript counterpart.
-if (Office.context.platform === Office.PlatformType.PC || Office.context.platform == null) {
-  Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
-}
+// if (Office.context.platform === Office.PlatformType.PC || Office.context.platform == null) {
+//  Office.actions.associate("onMessageSendHandler", onMessageSendHandler);
+// }
