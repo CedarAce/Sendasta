6da7ce52333ef78dcbc1085bd75a6f1cdea244b3
Mohamed Siblini <50118752+CedarAce@users.noreply.github.com>
2026-05-06 07:51:37 -0400

    Add setup guide for installing Sendasta in Outlook for IT admins

--- marketing/src/main.jsx
+++ marketing/src/main.jsx
@@ -4,10 +4,17 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')
+const app = (
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 )
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrateRoot(rootElement, app)
+} else {
+  ReactDOM.createRoot(rootElement).render(app)
+}
